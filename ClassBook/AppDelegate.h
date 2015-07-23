2d972c1b633ac81cdaeb63557d85c510c30f4b6f
jeetssoul <jitender.badlani@gmail.com>
2015-07-23 13:59:39 -0700

    Initial Commit

---
+++ ClassBook/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ClassBook
+//
+//  Created by Jitender Badlani on 7/23/15.
+//  Copyright (c) 2015 Jitender Badlani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
