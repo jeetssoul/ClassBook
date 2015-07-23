a836561cb451bd9e76c70c34d54666e6ac9021a7
jeetssoul <jitender.badlani@gmail.com>
2015-07-23 15:58:28 -0700

    Added person object

---
+++ ClassBook/Person.m
@@ -0,0 +1,21 @@
+//
+//  Person.m
+//  ClassBook
+//
+//  Created by Jitender Badlani on 7/23/15.
+//  Copyright (c) 2015 Jitender Badlani. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@synthesize firstName;
+@synthesize lastName;
+@synthesize address;
+@synthesize city;
+@synthesize state;
+@synthesize zip;
+@synthesize mobile;
+
+@end
