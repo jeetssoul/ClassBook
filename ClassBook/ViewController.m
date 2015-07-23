cf1e3fe6b578274d18959089b0686a32b7a54a29
jeetssoul <jitender.badlani@gmail.com>
2015-07-23 15:58:56 -0700

    Moved Alertview coz we don't gave fingerprint device

--- ClassBook/ViewController.m
+++ ClassBook/ViewController.m
@@ -7,9 +7,15 @@
 //
 
 #import "ViewController.h"
+#import "SVProgressHUD.h"
+#import "PeopleTableViewController.h"
+#import <LocalAuthentication/LocalAuthentication.h>
 
 @interface ViewController ()
 
+@property (nonatomic,strong) PeopleTableViewController *peopleTableViewController;
+
+//@property (strong, nonatomic) BlueViewController *blueViewController;
 @end
 
 @implementation ViewController
@@ -24,4 +30,48 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+#pragma mark - Button Press
+
+-(IBAction)authenticationButtonTapped:(id)sender
+{
+    LAContext *context = [[LAContext alloc] init];
+    NSError *error = nil;
+    
+    if([context canEvaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics error:&error])
+    {
+        [context evaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics localizedReason:@"Are you the device owner ?"
+                          reply:^(BOOL success, NSError *error) {
+                              if (error) {
+                                  [SVProgressHUD showErrorWithStatus:@"There was a problem"];
+                                  return;
+                              }
+                              if (success) {
+                                  [SVProgressHUD showErrorWithStatus:@"You are the owner"];
+                                  return;
+                              }
+                              else
+                              {
+                                  [SVProgressHUD showErrorWithStatus:@"You are not the device owner"];
+                                  return;
+                              }
+                              
+                          }];
+    }
+    else
+    {
+        /*ifUIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error"
+                                                        message:@"Your device cannot authenticate using TouchID"
+                                                       delegate:nil
+                                              cancelButtonTitle:@"OK"
+                                              otherButtonTitles:nil];
+        [alert show];*/
+        //[SVProgressHUD showErrorWithStatus:@"Your device cannot authenticate using TouchID"];
+        //UITableViewController *tableVC = [self.storyboard instantiateViewControllerWithIdentifier:@"People"];
+        self.peopleTableViewController.view.frame = self.view.frame;
+        //[self switchViewFromViewControlle toViewController:self.yellowViewController];
+        
+        
+    }
+}
 @end
